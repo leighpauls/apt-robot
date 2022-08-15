5e2ed5bcdb53109d39e0695d4bfb89038b9dfc95
leighpauls <leigh.pauls@gmail.com>
2022-08-14 18:15:44 -0700

    better serial_monitor

--- lidar_display/serial_monitor.py
+++ lidar_display/serial_monitor.py
@@ -12,9 +12,9 @@ def main() -> None:
     with serial.Serial(sys.argv[1], 19200, timeout=0) as ser:
         while True:
             # Flush the buffer
-            bytes_waiting = ser.in_waiting
-            print(f"{bytes_waiting} bytes waiting")
-            print(ser.read(bytes_waiting))
+            if bytes_waiting := ser.in_waiting:
+                print(f"{bytes_waiting} bytes waiting")
+                print(ser.read(bytes_waiting))
 
             time.sleep(0.1)
 
